13bb74334b013e5a7c41fe2edf3a60b5c9f57934
jiandi1234567890 <550392461@qq,com>
2016-05-06 09:28:16 +0800

    Initial Commit

---
+++ myscrollview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myscrollview
+//
+//  Created by 张海禄 on 16/5/6.
+//  Copyright © 2016年 张海禄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myscrollview/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  myscrollview
+//
+//  Created by 张海禄 on 16/5/6.
+//  Copyright © 2016年 张海禄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
